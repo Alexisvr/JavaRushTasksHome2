bc58a38583dc17b0021a7bf28f8799b667cd41b8
Алексей <alex18v@mail.ru>
2017-06-11 23:10:35 +0300

    Перенос с домашнего компьютера

---
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Horse.java
@@ -0,0 +1,50 @@
+package com.javarush.task.task21.task2113;
+
+/**
+ * Created by Алексей on 11.06.2017.
+ */
+public class Horse {
+    String name;
+    double speed, distance;
+
+    public Horse(String name, double speed, double distance) {
+        this.name = name;
+        this.speed = speed;
+        this.distance = distance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSpeed(double speed) {
+        this.speed = speed;
+    }
+
+    public void setDistance(double distance) {
+        this.distance = distance;
+    }
+
+    public void move() {
+        distance += (speed * Math.random());
+    }
+    public void print() {
+        for (int i = 1; i < distance; i++) {
+            System.out.print(".");
+        }
+        System.out.print(getName());
+        System.out.println();
+    }
+}
