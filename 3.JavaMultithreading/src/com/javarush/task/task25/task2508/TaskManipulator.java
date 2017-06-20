02beda50df83708edb312ad99f481b089e05cbeb
vorobev <alex18v@mail.ru>
2017-06-20 17:25:18 +0300

    Перенос задач с рабочего компьютера

---
+++ 3.JavaMultithreading/src/com/javarush/task/task25/task2508/TaskManipulator.java
@@ -0,0 +1,31 @@
+package com.javarush.task.task25.task2508;
+
+public class TaskManipulator implements CustomThreadManipulator, Runnable {
+
+    Thread thread;
+    String threadName;
+
+    @Override
+    public void run() {
+        try {
+            while (!thread.isInterrupted()) {
+                Thread.sleep(0);
+                System.out.println(threadName);
+                Thread.sleep(100);
+            }
+        } catch (InterruptedException e) {
+        }
+    }
+
+    @Override
+    public void start(String threadName) {
+        this.threadName = threadName;
+        thread = new Thread(this, threadName);
+        thread.start();
+    }
+
+    @Override
+    public void stop() {
+        thread.interrupt();
+    }
+}
